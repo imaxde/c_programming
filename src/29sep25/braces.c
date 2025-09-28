3dab4e2b7cec2d7f1a09d64cd11f00445fd9e5a0
Max Ipatov <st142148@student.spbu.ru>
2025-09-28 21:13:37 +0300

    Create braces checker

---
+++ src/29sep25/braces.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int check_str(char string[99])
+{
+    int balance = 0;
+    for (int i = 0; i < 99; i++) {
+        if (string[i] == '(')
+            balance += 1;
+        else if (string[i] == ')')
+            balance -= 1;
+        if (balance < 0)
+            return 0;
+    }
+    return balance == 0;
+}
+
+int main(void)
+{
+    char string[99];
+    scanf("%s", string);
+    int correct = check_str(string);
+    printf("%d\n", correct);
+}
