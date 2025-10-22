5d444e67c93500eb720dd0ff878d37281e584630
Max Ipatov <st142148@student.spbu.ru>
2025-10-22 23:35:11 +0300

    Remove char limit

--- src/29sep25/count.c
+++ src/29sep25/count.c
@@ -1,7 +1,7 @@
 #include <string.h>
 #include <stdio.h>
 
-int count(char str[100], char str1[100])
+int count(char *str, char *str1)
 {
     int size = strlen(str);
     int size1 = strlen(str1);
@@ -30,8 +30,8 @@ int count(char str[100], char str1[100])
 
 int main()
 {
-    char string[100] = "abcabc";
-    char string1[100] = "ab";
+    char string[7] = "abcabc";
+    char string1[3] = "ab";
     int result = count(string, string1);
     printf("Количество вхождений %d\n", result);
     return 0;
