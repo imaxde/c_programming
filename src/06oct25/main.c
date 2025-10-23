fecebc48342d83df170d817d3f2a68ca1e46be6c
Max Ipatov <st142148@student.spbu.ru>
2025-10-23 20:06:08 +0300

    Return code count changes

--- src/06oct25/main.c
+++ src/06oct25/main.c
@@ -5,12 +5,14 @@ int main(void)
 {
     int number;
     int data[100];
+    int original_data[100];
     int index = 0;
     
     printf("введите числа (для завершения нажмите enter+ctrl+d):\n");
 
     while (scanf("%d", &number) == 1) {
         data[index] = number;
+        original_data[index] = number;  // сохраняем копию исходного массива
         index++;
     };
 
@@ -21,5 +23,13 @@ int main(void)
         printf("%d ", data[i]);
     printf("\n");
 
-    return 0;
+    // подсчитываем различия
+    int moved_count = 0;
+    for (int i = 0; i < index; i++) {
+        if (data[i] != original_data[i]) {
+            moved_count++;
+        }
+    }
+
+    return moved_count;
 }
