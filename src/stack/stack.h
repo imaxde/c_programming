41bf3725186aca9bd31aff992b01c6c545761df8
Max Ipatov <st142148@student.spbu.ru>
2025-12-22 04:44:05 +0300

    Pop function get value

--- src/stack/stack.h
+++ src/stack/stack.h
@@ -9,7 +9,7 @@ struct Stack* new(void);
 struct Stack* push(struct Stack* stack, int value);
 
 // Берет элемент со стек, возвращает измененный стек, сохраняет удаленное значение в аргумент value
-struct Stack* pop(struct Stack* stack);
+struct Stack* pop(struct Stack* stack, int* value);
 
 // Возвращает верхний элемент стека
 int peek(struct Stack* stack);
