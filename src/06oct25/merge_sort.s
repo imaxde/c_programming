953eaac39957161981b9a4b1eea8177803208e1e
Max Ipatov <st142148@student.spbu.ru>
2025-12-22 03:17:14 +0300

    Update merge sort module

--- src/06oct25/merge_sort.s
+++ src/06oct25/merge_sort.s
@@ -1,252 +1,171 @@
 	.file	"merge_sort.c"
 	.text
 	.p2align 4
-	.globl	merge
-	.type	merge, @function
-merge:
-.LFB0:
+	.globl	mergeSort
+	.type	mergeSort, @function
+mergeSort:
+.LFB31:
 	.cfi_startproc
 	endbr64
-	pushq	%r12
+	pushq	%r15
 	.cfi_def_cfa_offset 16
-	.cfi_offset 12, -16
-	movq	%rsi, %r9
-	movq	%rdx, %r10
-	movl	%ecx, %r11d
-	pushq	%rbp
+	.cfi_offset 15, -16
+	pushq	%r14
 	.cfi_def_cfa_offset 24
-	.cfi_offset 6, -24
-	pushq	%rbx
+	.cfi_offset 14, -24
+	pushq	%r13
 	.cfi_def_cfa_offset 32
-	.cfi_offset 3, -32
-	movq	%rdi, %rbx
-	testl	%ecx, %ecx
-	jle	.L12
-	testl	%r8d, %r8d
-	jle	.L12
-	movl	$1, %ecx
-	xorl	%esi, %esi
-	xorl	%edi, %edi
-	jmp	.L5
-	.p2align 4,,10
-	.p2align 3
-.L28:
-	movl	%eax, %edx
-	addl	$1, %edi
-	leaq	1(%rcx), %rax
-	movl	%edx, -4(%r10,%rcx,4)
-	cmpl	%r11d, %edi
-	jge	.L2
-.L29:
-	cmpl	%r8d, %esi
-	jge	.L2
-	movq	%rax, %rcx
-.L5:
-	movslq	%edi, %rax
-	movslq	%esi, %rdx
-	movl	(%rbx,%rax,4), %eax
-	movl	(%r9,%rdx,4), %edx
-	cmpl	%eax, %edx
-	jge	.L28
-	movl	%edx, -4(%r10,%rcx,4)
-	addl	$1, %esi
-	leaq	1(%rcx), %rax
-	cmpl	%r11d, %edi
-	jl	.L29
-.L2:
-	cmpl	%edi, %r11d
-	jle	.L7
-	movslq	%edi, %rax
-	movl	%r11d, %ebp
-	leaq	(%rbx,%rax,4), %r12
-	subl	%edi, %ebp
-	movslq	%ecx, %rax
-	leaq	(%r10,%rax,4), %rbx
-	salq	$2, %rbp
-	xorl	%eax, %eax
-	.p2align 4,,10
-	.p2align 3
-.L8:
-	movl	(%r12,%rax), %edx
-	movl	%edx, (%rbx,%rax)
-	addq	$4, %rax
-	cmpq	%rax, %rbp
-	jne	.L8
-	addl	%ecx, %r11d
-	movl	%r11d, %ecx
-	subl	%edi, %ecx
-.L7:
-	cmpl	%esi, %r8d
+	.cfi_offset 13, -32
+	pushq	%r12
+	.cfi_def_cfa_offset 40
+	.cfi_offset 12, -40
+	movslq	%esi, %r12
+	pushq	%rbp
+	.cfi_def_cfa_offset 48
+	.cfi_offset 6, -48
+	pushq	%rbx
+	.cfi_def_cfa_offset 56
+	.cfi_offset 3, -56
+	subq	$40, %rsp
+	.cfi_def_cfa_offset 96
+	cmpl	$1, %r12d
 	jle	.L1
-	subl	%esi, %r8d
-	movslq	%esi, %rax
-	movslq	%ecx, %rcx
-	leaq	(%r9,%rax,4), %rsi
-	salq	$2, %r8
-	leaq	(%r10,%rcx,4), %rcx
-	xorl	%eax, %eax
+	movl	%r12d, %r15d
+	movq	%rdi, %r14
+	sarl	%r15d
+	movl	%r15d, %esi
+	call	mergeSort
+	movslq	%r15d, %rax
+	movl	%r12d, %r9d
+	subl	%r15d, %r9d
+	leaq	(%r14,%rax,4), %r8
+	movq	%r8, %rdi
+	movl	%r9d, %esi
+	movl	%r9d, 24(%rsp)
+	movq	%r8, 16(%rsp)
+	call	mergeSort
+	leaq	0(,%r12,4), %rax
+	movq	%rax, %rdi
+	movq	%rax, 8(%rsp)
+	call	malloc@PLT
+	movl	24(%rsp), %r9d
+	movq	16(%rsp), %r8
+	movq	%rax, %r12
+	testl	%r9d, %r9d
+	je	.L11
+	movl	$1, %edx
+	xorl	%ebp, %ebp
+	xorl	%r13d, %r13d
+	jmp	.L6
 	.p2align 4,,10
 	.p2align 3
+.L19:
+	movl	%esi, %eax
+	addl	$1, %r13d
+	movl	%eax, -4(%r12,%rdx,4)
+	leaq	1(%rdx), %rax
+	cmpl	%r13d, %r15d
+	jle	.L13
+.L20:
+	cmpl	%ebp, %r9d
+	jle	.L13
+	movq	%rax, %rdx
+.L6:
+	movslq	%r13d, %rax
+	movl	(%r14,%rax,4), %esi
+	movslq	%ebp, %rax
+	movl	(%r8,%rax,4), %eax
+	cmpl	%eax, %esi
+	jle	.L19
+	movl	%eax, -4(%r12,%rdx,4)
+	addl	$1, %ebp
+	leaq	1(%rdx), %rax
+	cmpl	%r13d, %r15d
+	jg	.L20
+.L13:
+	movl	%edx, %ebx
+	cmpl	%r13d, %r15d
+	jle	.L8
+.L3:
+	movslq	%ebx, %rax
+	movq	%r8, 24(%rsp)
+	leaq	(%r12,%rax,4), %rdi
+	leal	-1(%r15), %eax
+	addl	%ebx, %r15d
+	movl	%r9d, 16(%rsp)
+	subl	%r13d, %eax
+	movl	%r15d, %ebx
+	leaq	4(,%rax,4), %rdx
+	movslq	%r13d, %rax
+	subl	%r13d, %ebx
+	leaq	(%r14,%rax,4), %rsi
+	call	memcpy@PLT
+	movq	24(%rsp), %r8
+	movl	16(%rsp), %r9d
+.L8:
+	cmpl	%ebp, %r9d
+	jle	.L10
+	subl	$1, %r9d
+	movslq	%ebx, %rdx
+	movslq	%ebp, %rax
+	subl	%ebp, %r9d
+	leaq	(%r12,%rdx,4), %rdi
+	leaq	(%r8,%rax,4), %rsi
+	leaq	4(,%r9,4), %rdx
+	call	memcpy@PLT
 .L10:
-	movl	(%rsi,%rax), %edx
-	movl	%edx, (%rcx,%rax)
-	addq	$4, %rax
-	cmpq	%r8, %rax
-	jne	.L10
-.L1:
-	popq	%rbx
+	movq	8(%rsp), %rdx
+	movq	%r14, %rdi
+	movq	%r12, %rsi
+	call	memcpy@PLT
+	addq	$40, %rsp
 	.cfi_remember_state
-	.cfi_def_cfa_offset 24
+	.cfi_def_cfa_offset 56
+	movq	%r12, %rdi
+	popq	%rbx
+	.cfi_def_cfa_offset 48
 	popq	%rbp
-	.cfi_def_cfa_offset 16
+	.cfi_def_cfa_offset 40
 	popq	%r12
+	.cfi_def_cfa_offset 32
+	popq	%r13
+	.cfi_def_cfa_offset 24
+	popq	%r14
+	.cfi_def_cfa_offset 16
+	popq	%r15
+	.cfi_def_cfa_offset 8
+	jmp	free@PLT
+	.p2align 4,,10
+	.p2align 3
+.L1:
+	.cfi_restore_state
+	addq	$40, %rsp
+	.cfi_remember_state
+	.cfi_def_cfa_offset 56
+	popq	%rbx
+	.cfi_def_cfa_offset 48
+	popq	%rbp
+	.cfi_def_cfa_offset 40
+	popq	%r12
+	.cfi_def_cfa_offset 32
+	popq	%r13
+	.cfi_def_cfa_offset 24
+	popq	%r14
+	.cfi_def_cfa_offset 16
+	popq	%r15
 	.cfi_def_cfa_offset 8
 	ret
 	.p2align 4,,10
 	.p2align 3
-.L12:
+.L11:
 	.cfi_restore_state
-	xorl	%ecx, %ecx
-	xorl	%esi, %esi
-	xorl	%edi, %edi
-	jmp	.L2
+	xorl	%ebx, %ebx
+	xorl	%ebp, %ebp
+	xorl	%r13d, %r13d
+	jmp	.L3
 	.cfi_endproc
-.LFE0:
-	.size	merge, .-merge
-	.p2align 4
-	.globl	merge_sort
-	.type	merge_sort, @function
-merge_sort:
-.LFB1:
-	.cfi_startproc
-	endbr64
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	pushq	%r15
-	pushq	%r14
-	pushq	%r13
-	pushq	%r12
-	pushq	%rbx
-	subq	$56, %rsp
-	.cfi_offset 15, -24
-	.cfi_offset 14, -32
-	.cfi_offset 13, -40
-	.cfi_offset 12, -48
-	.cfi_offset 3, -56
-	movq	%fs:40, %rax
-	movq	%rax, -56(%rbp)
-	xorl	%eax, %eax
-	movq	%rsp, -72(%rbp)
-	cmpl	$1, %esi
-	jle	.L30
-	movl	%esi, %r15d
-	movl	%esi, %ecx
-	movq	%rsp, %rsi
-	movq	%rdi, %rbx
-	sarl	%r15d
-	movslq	%r15d, %r8
-	salq	$2, %r8
-	leaq	15(%r8), %rax
-	movq	%rax, %rdx
-	andq	$-4096, %rax
-	subq	%rax, %rsi
-	andq	$-16, %rdx
-	cmpq	%rsi, %rsp
-	je	.L34
-.L48:
-	subq	$4096, %rsp
-	orq	$0, 4088(%rsp)
-	cmpq	%rsi, %rsp
-	jne	.L48
-.L34:
-	andl	$4095, %edx
-	subq	%rdx, %rsp
-	testq	%rdx, %rdx
-	jne	.L49
-.L35:
-	movl	%ecx, %r13d
-	movq	%rsp, %rsi
-	movq	%rsp, %r14
-	subl	%r15d, %r13d
-	movslq	%r13d, %rax
-	leaq	15(,%rax,4), %rax
-	movq	%rax, %rdx
-	andq	$-4096, %rax
-	andq	$-16, %rdx
-	subq	%rax, %rsi
-.L36:
-	cmpq	%rsi, %rsp
-	je	.L37
-	subq	$4096, %rsp
-	orq	$0, 4088(%rsp)
-	jmp	.L36
-.L37:
-	andl	$4095, %edx
-	subq	%rdx, %rsp
-	testq	%rdx, %rdx
-	je	.L38
-	orq	$0, -8(%rsp,%rdx)
-.L38:
-	movl	%r15d, %edx
-	movq	%rbx, %rsi
-	movq	%r14, %rdi
-	movl	%ecx, -84(%rbp)
-	salq	$2, %rdx
-	movq	%r8, -80(%rbp)
-	movq	%rsp, %r12
-	call	memcpy@PLT
-	movl	-84(%rbp), %ecx
-	cmpl	%r15d, %ecx
-	jle	.L39
-	movq	-80(%rbp), %r8
-	subl	$1, %ecx
-	movq	%r12, %rdi
-	subl	%r15d, %ecx
-	leaq	4(,%rcx,4), %rdx
-	leaq	(%rbx,%r8), %rsi
-	call	memcpy@PLT
-.L39:
-	movl	%r15d, %esi
-	movq	%r14, %rdi
-	call	merge_sort
-	movl	%r13d, %esi
-	movq	%r12, %rdi
-	call	merge_sort
-	movl	%r13d, %r8d
-	movl	%r15d, %ecx
-	movq	%rbx, %rdx
-	movq	%r12, %rsi
-	movq	%r14, %rdi
-	call	merge
-	movq	-72(%rbp), %rsp
-.L30:
-	movq	-56(%rbp), %rax
-	subq	%fs:40, %rax
-	jne	.L50
-	leaq	-40(%rbp), %rsp
-	popq	%rbx
-	popq	%r12
-	popq	%r13
-	popq	%r14
-	popq	%r15
-	popq	%rbp
-	.cfi_remember_state
-	.cfi_def_cfa 7, 8
-	ret
-	.p2align 4,,10
-	.p2align 3
-.L49:
-	.cfi_restore_state
-	orq	$0, -8(%rsp,%rdx)
-	jmp	.L35
-.L50:
-	call	__stack_chk_fail@PLT
-	.cfi_endproc
-.LFE1:
-	.size	merge_sort, .-merge_sort
+.LFE31:
+	.size	mergeSort, .-mergeSort
 	.ident	"GCC: (Ubuntu 13.3.0-6ubuntu2~24.04) 13.3.0"
 	.section	.note.GNU-stack,"",@progbits
 	.section	.note.gnu.property,"a"
