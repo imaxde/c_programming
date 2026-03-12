86ae5200aca2f21b75e8fb84ad54b2d7d15a6e95
Max Ipatov <st142148@student.spbu.ru>
2026-03-12 13:46:52 +0300

    fix variables names

--- src/23oct25/sorting_station.c
+++ src/23oct25/sorting_station.c
@@ -1,132 +1,192 @@
 #include <ctype.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "../stack/stack.h"
 
 int getPrecedence(char op)
 {
-    if (op == '*' || op == '/') {
+    if (op == '*' || op == '/')
         return 2;
-    }
 
-    if (op == '+' || op == '-') {
+    if (op == '+' || op == '-')
         return 1;
-    }
 
     return 0;
 }
 
-void infixToPostfix(const char* infix, char* postfix)
+bool appendPostfixChar(char* postfix, size_t postfixCapacity, size_t* postfixIndex, char token)
 {
-    struct Stack* stack = new();
-    int i = 0;
-    int j = 0;
-    int topVal = 0;
-    int val = 0;
+    if (*postfixIndex + 1 >= postfixCapacity)
+        return false;
 
-    while (infix[i] != '\0') {
-        char token = infix[i];
+    postfix[*postfixIndex] = token;
+    (*postfixIndex)++;
+    return true;
+}
 
-        if (isspace(token)) {
-            i++;
+bool appendPostfixToken(char* postfix, size_t postfixCapacity, size_t* postfixIndex, char token)
+{
+    if (!appendPostfixChar(postfix, postfixCapacity, postfixIndex, token))
+        return false;
+
+    return appendPostfixChar(postfix, postfixCapacity, postfixIndex, ' ');
+}
+
+bool infixToPostfix(char* infix, char* postfix, size_t postfixCapacity)
+{
+    Stack* stack = newStack();
+    size_t infixIndex = 0;
+    size_t postfixIndex = 0;
+    bool isValid = true;
+
+    if (stack == NULL || postfixCapacity == 0) {
+        if (postfixCapacity > 0) {
+            postfix[0] = '\0';
+        }
+        return false;
+    }
+
+    while (infix[infixIndex] != '\0' && isValid) {
+        char token = infix[infixIndex];
+
+        if (isspace((unsigned char)token)) {
+            infixIndex++;
             continue;
         }
 
-        if (isdigit(token)) {
-            while (isdigit(infix[i])) {
-                postfix[j++] = infix[i++];
-            }
-            postfix[j++] = ' ';
+        if (isdigit((unsigned char)token)) {
+            isValid = appendPostfixToken(postfix, postfixCapacity, &postfixIndex, token);
+            infixIndex++;
             continue;
         }
 
         if (token == '(') {
-            stack = push(stack, token);
-        } else if (token == ')') {
-            while (stack != NULL && peek(stack) != '(') {
-                stack = pop(stack, &topVal);
-                postfix[j++] = (char)topVal;
-                postfix[j++] = ' ';
-            }
-
-            if (stack != NULL && peek(stack) == '(') {
-                stack = pop(stack, &topVal);
-            }
-        } else if (token == '+' || token == '-' || token == '*' || token == '/') {
-            while (stack != NULL) {
-                int top = peek(stack);
-                if (top == '(') {
-                    break;
-                }
-
-                if (getPrecedence((char)top) >= getPrecedence(token)) {
-                    stack = pop(stack, &val);
-                    postfix[j++] = (char)val;
-                    postfix[j++] = ' ';
-                } else {
-                    break;
-                }
-            }
-
-            stack = push(stack, token);
+            push(stack, token);
+            infixIndex++;
+            continue;
         }
 
-        i++;
+        if (token == ')') {
+            bool hasOpeningParen = false;
+            int stackTop = 0;
+
+            while (peek(stack, &stackTop)) {
+                pop(stack, &stackTop);
+
+                if (stackTop == '(') {
+                    hasOpeningParen = true;
+                    break;
+                }
+
+                if (!appendPostfixToken(postfix, postfixCapacity, &postfixIndex, (char)stackTop)) {
+                    isValid = false;
+                    break;
+                }
+            }
+
+            if (!hasOpeningParen)
+                isValid = false;
+
+            infixIndex++;
+            continue;
+        }
+
+        if (token == '+' || token == '-' || token == '*' || token == '/') {
+            int stackTop = 0;
+
+            while (peek(stack, &stackTop) && stackTop != '('
+                && getPrecedence((char)stackTop) >= getPrecedence(token)) {
+                pop(stack, &stackTop);
+
+                if (!appendPostfixToken(postfix, postfixCapacity, &postfixIndex, (char)stackTop)) {
+                    isValid = false;
+                    break;
+                }
+            }
+
+            if (isValid) 
+                push(stack, token);
+
+            infixIndex++;
+            continue;
+        }
+
+        isValid = false;
+        infixIndex++;
     }
 
-    while (stack != NULL) {
-        stack = pop(stack, &val);
-        postfix[j++] = (char)val;
-        postfix[j++] = ' ';
+    while (isValid) {
+        int stackTop = 0;
+
+        if (!peek(stack, &stackTop))
+            break;
+
+        pop(stack, &stackTop);
+
+        if (stackTop == '(') {
+            isValid = false;
+            break;
+        }
+
+        if (!appendPostfixToken(postfix, postfixCapacity, &postfixIndex, (char)stackTop)) {
+            isValid = false;
+            break;
+        }
     }
 
-    if (j > 0 && postfix[j - 1] == ' ') {
-        postfix[j - 1] = '\0';
-    } else {
-        postfix[j] = '\0';
-    }
+    if (isValid && postfixIndex > 0) 
+        postfixIndex--;
 
-    delete(stack);
+    postfix[isValid ? postfixIndex : 0] = '\0';
+
+    deleteStack(stack);
+    return isValid;
 }
 
 int main(void)
 {
     char* infix = NULL;
     char* postfix = NULL;
-    size_t capacity = 0;
-    size_t length = 0;
+    size_t infixCapacity = 0;
+    size_t infixLength = 0;
     int ch = 0;
 
     while ((ch = getchar()) != '\n' && ch != EOF) {
-        if (length + 1 >= capacity) {
-            if (capacity == 0) {
-                capacity = 16;
+        if (infixLength + 1 >= infixCapacity) {
+            if (infixCapacity == 0) {
+                infixCapacity = 16;
             } else {
-                capacity *= 2;
+                infixCapacity *= 2;
             }
 
-            infix = realloc(infix, capacity * sizeof(char));
-            if (infix == NULL) {
+            char* temp = realloc(infix, infixCapacity * sizeof(char));
+            if (temp == NULL) {
+                free(infix);
                 return 1;
             }
+            infix = temp;
         }
-        infix[length++] = (char)ch;
+        infix[infixLength++] = (char)ch;
     }
 
     if (infix != NULL) {
-        infix[length] = '\0';
+        infix[infixLength] = '\0';
     } else {
         infix = calloc(1, sizeof(char));
+        if (infix == NULL) {
+            return 1;
+        }
     }
 
-    postfix = calloc(length * 2 + 1, sizeof(char));
+    size_t postfixCapacity = infixLength * 2 + 1;
+    postfix = calloc(postfixCapacity, sizeof(char));
     if (postfix == NULL) {
         free(infix);
         return 1;
     }
 
-    infixToPostfix(infix, postfix);
+    infixToPostfix(infix, postfix, postfixCapacity);
     printf("%s\n", postfix);
 
     free(infix);
